cd6999a4a0fb13392aa12c8f42f9e9d7a76b4799
shawnsandy04 <shawnsandy04@gmail.com>
2017-09-04 20:40:00 -0400

    package - publish vendor assets and files

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page::index');
+});
+
+
+Route::group(["prefix" => "page"], function(){
+   Pages::routes();
 });
